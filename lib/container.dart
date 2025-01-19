f623c79b33a01d648d85357cabd0b371a9a0b8ab
“Nabeel <0000>
2025-01-19 09:51:32 +0500

    Container Decoration

--- lib/container.dart
+++ lib/container.dart
@@ -9,52 +9,56 @@ class MyContainer extends StatelessWidget {
       appBar:AppBar(centerTitle:true,
         title:const Text("Container"),
       ),
-      body:Row(
-        mainAxisAlignment:MainAxisAlignment.center,
+      //We take column to pass multiple widgets because column accept children
+      body:Column(
+        crossAxisAlignment:CrossAxisAlignment.center,
         children: [
-          Column(
-            crossAxisAlignment:CrossAxisAlignment.center,
-            children: [
-              Container(
-                height:200,
-                width:200,
-                color:Colors.blue,
-              ),
-              Container(
-                height:200,
-                width:200,
-               decoration: BoxDecoration(
-                 color:Colors.red,
-                 borderRadius:BorderRadius.circular(30)
-               ),
-              ),
-              Container(
-                height:200,
-                width:200,
-                decoration: BoxDecoration(
-                    color:Colors.white,
-                    borderRadius:BorderRadius.circular(30),
-                  border:Border.all(
-                    color:Colors.black
-                  )
+          Container(
+            height:200,
+            width:200,
+            color:Colors.blue,
+          ),
+          Container(
+            height:200,
+            width:200,
+           decoration: BoxDecoration(
+             color:Colors.red,
+             borderRadius:BorderRadius.circular(30)
+           ),
+          ),
+          Container(
+            height:200,
+            width:200,
+            decoration: BoxDecoration(
+                color:Colors.white,
+                borderRadius:BorderRadius.circular(30),
+              border:Border.all(
+                color:Colors.black
+              )
+            ),
+          ),
+          Container(
+            height:200,
+            width:200,
+            decoration: BoxDecoration(
+                color:Colors.white,
+                borderRadius:BorderRadius.circular(30),
+              boxShadow:const [
+                BoxShadow(
+                  //off direction of shadow
+                  offset:Offset(0, 2),
+                  //color shadow color
+                  color:Colors.black,
+                  //create blur below the container
+                  blurRadius:10
                 ),
-              ),
-              Container(
-                height:200,
-                width:200,
-                decoration: BoxDecoration(
-                    color:Colors.white,
-                    borderRadius:BorderRadius.circular(30),
-                  boxShadow:const [
-                    BoxShadow(
-                      offset:Offset(0, 2),
-                      color:Colors.black,
-                      blurRadius:3
-                    )
-                  ]
-                ),
-              ),
-            ],
+                // BoxShadow(
+                //     offset:Offset(0, 2),
+                //     color:Colors.black,
+                //     blurRadius:3
+                // )
+              ]
+            ),
           ),
         ],
       ),
